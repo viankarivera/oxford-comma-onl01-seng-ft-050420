dd7ac87bc118761e4bd31ee1c6fa0f9bce2f94b5
Vianka Rivera <viankalrivera@gmail.com>
2020-05-07 16:47:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
-  new_array = [array].join
+#def oxford_comma(array)
+  #new_array = [array].join
+#end
+
+  def oxford_comma(array)
+    if array.length == 2
+    array[-2] << " and "
+    array.join
+    elsif array.length == 1
+    	array.join
+    elsif array.length > 2
+      array[-1].prepend "and "
+  	array.join(", ")
+    end
   end
